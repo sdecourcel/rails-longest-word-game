3482e81c335cfb90ce023b89d4ad38339518230d
Sibylle de Courcel <sibylle44@gmail.com>
2017-02-13 16:08:49 +0100

    wooouh

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get '/game' => 'playgame#game', as: :game
+
+  get '/score' => 'playgame#score', as: :score
+
+  root 'playgame#game'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
